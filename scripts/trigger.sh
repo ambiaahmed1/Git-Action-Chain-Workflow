32e7cc7f6be6ba10d7ca742b232670bb2feb9a90
ambiaahmed1 <ambiaahmed1@gmail.com>
2023-11-02 02:26:28 -0400

    test 4

--- scripts/trigger.sh
+++ scripts/trigger.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-curl -X POST -H "Authorization: Bearer $EXTERNAL_PAT" \
+set -ex
+curl -X POST -H "Authorization: Bearer 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" \
   -H "Accept: application/vnd.github.everest-preview+json" \
   -H "Content-Type: application/json" \
-  https://api.github.com/ambiaahmed1/Selenium-Git-CI-Solution.git/dispatches \
+  https://api.github.com/ambiaahmed1/Selenium-Git-CI-Solution.git \
   --data '{"event_type": "trigger-workflow"}'
